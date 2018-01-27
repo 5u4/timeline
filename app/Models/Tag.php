35900ee1adceb52f37a54b17b167d028b56a2927
Senhung <alexwongsenhung@gmail.com>
2018-01-26 22:15:30 -0800

    Tag Model + Migration

--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -4,7 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property integer $id
+ * @property string $name
+ * @property integer $user_id
+ *
+ * Class Tag
+ * @package App\Models
+ */
 class Tag extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'user_id',
+    ];
 }
--- database/migrations/2018_01_27_053030_create_tags_table.php
+++ database/migrations/2018_01_27_053030_create_tags_table.php
@@ -14,8 +14,12 @@ class CreateTagsTable extends Migration
     public function up()
     {
         Schema::create('tags', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id')->unsigned();
+            $table->string('name');
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
         });
     }
 
