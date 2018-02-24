847485dcf910630b101d9bb5e214eda7ff7850d2
Senhung Wong <alexwongsenhung@gmail.com>
2018-02-23 23:38:43 -0800

    TL-9: Tag Model

---
+++ app/Models/Tag.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Tag
+ * @package App\Models
+ *
+ * @property int id
+ * @property int user_id
+ * @property string name
+ * @property string color
+ */
+class Tag extends Model
+{
+    protected $table = 'tags';
+
+    public $primaryKey = 'id';
+
+    public $timestamps = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'name', 'description', 'date', 'done',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User', 'user_id', 'id');
+    }
+}
